9cfcbfcee6255b402a57f20dc916c86bfc23c8fb
Hos9 <shahin9315@gmail.com>
2026-03-19 23:00:49 +0600

    Flag, Name & Populations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Suspense } from "react";
 import "./App.css";
 import Countries from "./Components/Countries/Countries";
+import Country from "./Components/Country/Country";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all",
+).then((res) => res.json());
 
 function App() {
   return (
     <>
-      {/* <h1>Hos9 on the Go..........!!</h1> */}
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir Loading . . . </p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+
+      {/* <Suspense fallback={<p>Countries are loading . . . .</p>}>
+        <Country></Country>
+      </Suspense> */}
     </>
   );
 }
