5137121e3b9f334190aebee52ef75f1250bb7b28
Hos9 <shahin9315@gmail.com>
2026-03-19 16:14:07 +0600

    Ont he go initiated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./Components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hos9 on the Go..........!!</h1> */}
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
